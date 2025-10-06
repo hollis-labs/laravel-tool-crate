3e5bcb424387ce1c36e33096672747875a9aceb4
Chrispian <chrispian@gmail.com>
2025-10-06 00:50:36 -0500

    Finished removing the Orchestration commands

--- src/Servers/ToolCrateServer.php
+++ src/Servers/ToolCrateServer.php
@@ -10,25 +10,12 @@ use HollisLabs\ToolCrate\Tools\FileReadTool;
 use HollisLabs\ToolCrate\Tools\TextReplaceTool;
 use HollisLabs\ToolCrate\Tools\HelpIndexTool;
 use HollisLabs\ToolCrate\Tools\HelpToolDetail;
-use HollisLabs\ToolCrate\Tools\Orchestration\AgentsListTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\AgentDetailTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\AgentSaveTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\AgentStatusTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\SprintsListTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\TaskAssignTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\TaskDetailTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\TasksListTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\TaskStatusTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\SprintDetailTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\SprintSaveTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\SprintStatusTool;
-use HollisLabs\ToolCrate\Tools\Orchestration\SprintTasksAttachTool;
 
 class ToolCrateServer extends Server
 {
     protected string $name = 'ToolCrate';
     protected string $version = '0.2.0';
-    protected string $instructions = 'Local dev toolbox. Use help.index to discover JSON, search, file, and orchestration helpers.';
+    protected string $instructions = 'Local dev toolbox. Use help.index to discover JSON, search, and file helpers.';
 
     protected array $tools = [
         JqQueryTool::class,
@@ -37,19 +24,6 @@ class ToolCrateServer extends Server
         TextReplaceTool::class,
         HelpIndexTool::class,
         HelpToolDetail::class,
-        AgentsListTool::class,
-        AgentDetailTool::class,
-        AgentSaveTool::class,
-        AgentStatusTool::class,
-        SprintsListTool::class,
-        TasksListTool::class,
-        TaskDetailTool::class,
-        TaskAssignTool::class,
-        TaskStatusTool::class,
-        SprintDetailTool::class,
-        SprintSaveTool::class,
-        SprintStatusTool::class,
-        SprintTasksAttachTool::class,
     ];
 
     public function __construct(Transport $transport)
@@ -76,19 +50,6 @@ class ToolCrateServer extends Server
             TextReplaceTool::class => 'text.replace',
             HelpIndexTool::class => 'help.index',
             HelpToolDetail::class => 'help.tool',
-            AgentsListTool::class => 'orchestration.agents.list',
-            AgentDetailTool::class => 'orchestration.agents.detail',
-            AgentSaveTool::class => 'orchestration.agents.save',
-            AgentStatusTool::class => 'orchestration.agents.status',
-            SprintsListTool::class => 'orchestration.sprints.list',
-            TasksListTool::class => 'orchestration.tasks.list',
-            TaskDetailTool::class => 'orchestration.tasks.detail',
-            TaskAssignTool::class => 'orchestration.tasks.assign',
-            TaskStatusTool::class => 'orchestration.tasks.status',
-            SprintDetailTool::class => 'orchestration.sprints.detail',
-            SprintSaveTool::class => 'orchestration.sprints.save',
-            SprintStatusTool::class => 'orchestration.sprints.status',
-            SprintTasksAttachTool::class => 'orchestration.sprints.attach_tasks',
         ];
 
         return $map[$toolClass] ?? '';
